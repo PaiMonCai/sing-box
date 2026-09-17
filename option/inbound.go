49691f6c924c64d2a545df2dee3d57087ffff54a
派蒙！ <paimoncai.0@gmail.com>
2026-09-17 22:24:27 +0800

    feat(listener): restore trusted proxy protocol options

--- option/inbound.go
+++ option/inbound.go
@@ -74,10 +74,10 @@ type ListenOptions struct {
 	UDPTimeout           UDPTimeoutCompat   `json:"udp_timeout,omitempty"`
 	Detour               string             `json:"detour,omitempty"`
 
-	// Deprecated: removed
-	ProxyProtocol bool `json:"proxy_protocol,omitempty"`
-	// Deprecated: removed
-	ProxyProtocolAcceptNoHeader bool `json:"proxy_protocol_accept_no_header,omitempty"`
+	// TX extension: Proxy Protocol is only accepted from explicitly trusted peers.
+	ProxyProtocol               bool     `json:"proxy_protocol,omitempty"`
+	ProxyProtocolAcceptNoHeader bool     `json:"proxy_protocol_accept_no_header,omitempty"`
+	ProxyProtocolTrustedCIDRs   []string `json:"proxy_protocol_trusted_cidrs,omitempty"`
 	InboundOptions
 }
 
